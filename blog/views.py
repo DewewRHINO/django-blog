496eeb02e98f8589707c92ccfb56a14e076c8939
Jacob Jayme <jaymerjacob@Jacobs-MacBook-Air.local>
2025-07-12 17:34:01 -0700

    Added blog post template

--- blog/views.py
+++ blog/views.py
@@ -3,11 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 from .models import Post
+from django.template import loader
 
 def index(request):
     latest_post_list = Post.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.post_text for q in latest_post_list])
-    return HttpResponse(output)
+    template = loader.get_template("blog/index.html")
+    context = {"latest_post_list": latest_post_list}
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at Blog %s." % question_id)
