8d7bbb5aadd99b88689f72d533777feb00e1a840
Jacob Jayme <jaymerjacob@Jacobs-MacBook-Air.local>
2025-07-12 17:13:55 -0700

    Added Posts model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    post_text = models.CharField(max_length=200000)
+    pub_date = models.DateTimeField("date published")
