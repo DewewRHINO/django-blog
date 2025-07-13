f8b825b356e8783fc7e88618dbd52b67b4fc1e66
Jacob Jayme <jaymerjacob@Jacobs-MacBook-Air.local>
2025-07-12 17:28:50 -0700

    Added Blog Post Title field

--- blog/models.py
+++ blog/models.py
@@ -7,11 +7,12 @@ from django.utils import timezone
 # Create your models here.
 
 class Post(models.Model):
+    post_title = models.CharField(max_length=100)
     post_text = models.CharField(max_length=200000)
     pub_date = models.DateTimeField("date published")
     
     def __str__(self):
-        return self.post_text
+        return self.post_title
     
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
