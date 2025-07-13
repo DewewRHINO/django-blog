b2339216fbd58560d3aaf6f270a6461c8c127e0c
Jacob Jayme <jaymerjacob@Jacobs-MacBook-Air.local>
2025-07-12 17:55:17 -0700

    Added 404 Errors

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-
 from . import views
 
+
+app_name = "blog" # Creating a namespace so I can use the url building stuff
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:post_id>/", views.detail, name="detail"),
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
 # Create your views here.
 from django.http import HttpResponse
 from .models import Post
 from django.template import loader
 from django.http import Http404
-
 
 def index(request):
     latest_post_list = Post.objects.order_by("-pub_date")[:5]
@@ -14,10 +13,10 @@ def index(request):
     return HttpResponse(template.render(context, request))
 
 def detail(request, post_id):
+    # try: 
+    #     post = Post.objects.get(pk=post_id)
+    # except Post.DoesNotExist:
+    #     raise Http404("That post does not exist.")
     
-    try: 
-        post = Post.objects.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404("That post does not exist.")
-    
+    post = get_object_or_404(Post, pk=post_id)
     return render(request,"blog/post.html",{"post":post})
