68f03692966df3816a3b347442756078d90a9fac
Jacob Jayme <jaymerjacob@Jacobs-MacBook-Air.local>
2025-07-12 17:24:22 -0700

    Added Views for blogs

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django import forms
+import datetime
+from django.utils import timezone
+
 
 # Create your models here.
 
 class Post(models.Model):
     post_text = models.CharField(max_length=200000)
     pub_date = models.DateTimeField("date published")
+    
+    def __str__(self):
+        return self.post_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
